f20d769aa8bd5613b05bf90d09303cdab2f1e2bf
Vladimir <vladimir.pinchuk01@gmail.com>
2020-07-10 18:59:26 +0300

    tinychargeamp: remove division

--- piezo-probe/tinychargeamp/main.c
+++ piezo-probe/tinychargeamp/main.c
@@ -15,7 +15,6 @@ void pwm_init() {
 
 void init() {
     _delay_ms(1000);
-    //DDRB = (1<<1); // led pin: output 
     ADMUX = (1 << REFS0) | (1 << MUX1) | (1 << MUX0);
     ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS1) | (1 << ADPS2) | (1 << ADPS0);
     ADCSRB = (1 << ADTS1) | (1 << ADTS0);
@@ -37,12 +36,10 @@ void noise_reduce_return() {
     power_timer0_enable();
 }
 
-ISR(ADC_vect) {
-    noise_reduce_return();
-}
+EMPTY_INTERRUPT(ADC_vect);
 
 
-int32_t adc_read0() {
+long adc_read0() {
     noise_reduce();
     _delay_us(100);
     sleep_cpu();
@@ -50,7 +47,7 @@ int32_t adc_read0() {
     return ADC;
 }
 
-int32_t measure_offset() {
+long measure_offset() {
     uint8_t old_mux = ADMUX;
     ADMUX = (1 << REFS0) | (1 << MUX1);
     adc_read0();
@@ -73,11 +70,9 @@ int main() {
     while (1) {
         long reading = adc_read0() << 5; // 15 bits (0 .. 32767)
         long rate = average - reading;
-        integral =- integral / 10;
+        integral =- integral >> 3;
         integral += rate;
         
-        
-        //led(integral > 2000);
         if (integral > 9000) {
             led(1);
             _delay_ms(1);
